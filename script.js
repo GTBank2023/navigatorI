e4ec6664444bd02fba7ce641194551d39ce263a2
GTBank2023 <140178184+GTBank2023@users.noreply.github.com>
2023-10-13 10:17:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -510,7 +510,7 @@ console.log('Handling of the areas detected');
 }
 
 async function detectObjects() {
-  const canvas = document.getElementById('canvas');
+const canvas = document.getElementById('CanvasId');
   const ctx = canvas.getContext('2d'); // Define ctx here
 
   // Ensure ctx is defined
@@ -1325,8 +1325,8 @@ console.log('Handling of the areas detected');
 }
 
 async function detectObjects(tensor) {
-  const canvas = document.getElementById('canvas');
-  const ctx = canvas.getContext('2d'); // Define ctx here
+const canvas = document.getElementById('CanvasId');
+ const ctx = canvas.getContext('2d'); // Define ctx here
 
   // Ensure ctx is defined
   if (!ctx) {
